caad13d2bebcf4f29622428b5d44035654bbb92b
roxlabsystems <roxlabsystems@gmail.com>
2025-11-04 23:58:52 +0300

    Update minor changes

--- src/pages/Home/PropertyFeatures.jsx
+++ src/pages/Home/PropertyFeatures.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-import './propertyfeatures.css'
+import './PropertyFeatures.css'
 
 const PropertyFeatures = (props) => {
   const [activeTab, setActiveTab] = useState(0)
