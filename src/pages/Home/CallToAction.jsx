f5eea25616fd171e013118845dd414512ba1add7
roxlabsystems <roxlabsystems@gmail.com>
2025-11-05 00:33:08 +0300

    deploy errors sorted

--- src/pages/Home/CallToAction.jsx
+++ src/pages/Home/CallToAction.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../../components/ui/Button";
-import "./CallToAction.css";g
+import "./CallToAction.css";
 
 const CallToAction = () => {
   const navigate = useNavigate();
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'build',
+    sourcemap: true
   },
   resolve: {
     alias: {
